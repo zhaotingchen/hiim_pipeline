a4c3cd4f1a12008ccec7a59691b61b3c3396fa57
Zhaoting Chen <ztchenustc@gmail.com>
2023-09-12 03:14:06 +0200

    add short bl flag to 1GC pre flag

--- interim/1GC_00_pre_flag.py
+++ interim/1GC_00_pre_flag.py
@@ -13,6 +13,26 @@ for REPHASE in CAL_1GC_REPHASE:
     if REPHASE:
         raise ValueError('Field Dir not matching the primary calibrator position')
 
+        
+# ------------------------------------------------------------------------
+# Frequency ranges to flag over short baselines
+if CAL_1GC_bl_spw!='':
+    spw_bl = '*:'
+    for spw in CAL_1GC_bl_spw.split(','):
+        spw_bl += spw.split(':')[-1]+';'
+    spw_bl = spw_bl[:-1]
+    field_name = ''
+    for field in CAL_1GC_PRIMARY_NAME:
+        field_name += field+','
+    field_name = field_name[:-1]
+    flagdata(vis = mymms,
+		mode = 'manual', 
+        field = field_name,
+		spw = spw_bl,
+        flagbackup=False,
+		uvrange = '0~600m'
+            )
+
 print('Flagging autocorr...')
 #flag autocorr
 flagdata(vis = mymms,
@@ -36,14 +56,14 @@ flagdata(vis = mymms,
          flagbackup=False,
         )
 
-print('Adding model column...')
-clearcal(vis = mymms, addmodel = True)
+#print('Adding model column...')
+#clearcal(vis = mymms, addmodel = True)
 
 if save_flag:
     print('Saving flags...')
     flagmanager(vis = mymms,
                 mode = 'save',
-                versionname = 'basic')
+                versionname = 'pre_1GC')
     
 clearstat()
 clearstat()
