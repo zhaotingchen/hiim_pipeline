405a70057ff2260ca417ed1a05b63b817994f466
Zhaoting Chen <ztchenustc@gmail.com>
2024-01-30 17:53:13 +0200

    add pol state in

--- interim/0GC_02_get_state.py
+++ interim/0GC_02_get_state.py
@@ -19,26 +19,35 @@ config = tidy_config_path(config)
 config_path = config['FILE']['work_dir']
 sys.path.append(config_path)
 from config import FILE_working_ms,FILE_hiimtool,CAL_1GC_primary_intent,CAL_1GC_secondary_intent,CAL_1GC_target_intent
+try:
+    from config import CAL_1G_polarization_intent
+    has_pol_cal = True
+except:
+    has_pol_cal = False
 
 
-sys.path.append(FILE_hiimtool)
 from hiimtool.ms_tool import get_nchan,get_nscan,get_fields,get_states
-from hiimtool.ms_tool import get_primary_candidates,get_secondaries,get_targets,get_primary_tag
-from hiimtool.basic_util import vfind_scan
-
-def unravel_list(inp):
-    out = [item for sublist in inp for item in sublist]
-    return out
+from hiimtool.ms_tool import get_primary_candidates,get_secondaries,get_targets,get_primary_tag,get_polarizations
+from hiimtool.basic_util import vfind_scan,unravel_list
 
 ms_dir = FILE_working_ms
 
-primary_state, secondary_state, target_state, unknown_state =get_states(
-    ms_dir,
-    CAL_1GC_primary_intent,
-    CAL_1GC_secondary_intent,
-    CAL_1GC_target_intent,
-)
-
+if has_pol_cal:
+    primary_state, secondary_state, target_state, unknown_state,pol_state =get_states(
+        ms_dir,
+        CAL_1GC_primary_intent,
+        CAL_1GC_secondary_intent,
+        CAL_1GC_target_intent,
+        CAL_1G_polarization_intent
+    )
+else:
+    primary_state, secondary_state, target_state, unknown_state =get_states(
+        ms_dir,
+        CAL_1GC_primary_intent,
+        CAL_1GC_secondary_intent,
+        CAL_1GC_target_intent,
+    )
+    
 field_dirs,field_names,field_ids = get_fields(ms_dir)
 
 candidate_dirs, candidate_names, candidate_ids = get_primary_candidates(
@@ -50,6 +59,14 @@ primary_name,primary_id,primary_tag,primary_sep = get_primary_tag(candidate_dirs
                 candidate_ids)
 
 secondary_dirs, secondary_names, secondary_ids = get_secondaries(ms_dir,secondary_state,field_dirs,field_names,field_ids)
+
+
+target_dirs, target_names, target_ids = get_targets(ms_dir,target_state,field_dirs,field_names,field_ids)
+
+if has_pol_cal:
+    pol_dirs, pol_names, pol_ids = get_polarizations(ms_dir,pol_state,field_dirs,field_names,field_ids)
+else:
+    pol_names = []
 
 dorephrase = (np.array(primary_sep)>1e-5).tolist()
 
@@ -67,6 +84,13 @@ secondary_scan = []
 for i in range(len(secondary_state)):
     secondary_scan += [[],]
 
+pol_scan = []
+if has_pol_cal:
+    for i in range(len(pol_state)):
+        pol_scan += [[],]
+
+target_scan = []
+
 for i,scan_id in enumerate(scan_list):
     sub_ms = subms_list[i]
     tab = table(sub_ms,ack=False)
@@ -75,10 +99,11 @@ for i,scan_id in enumerate(scan_list):
         primary_scan[primary_state.index(sub_state)] += (scan_id,)
     if sub_state in secondary_state:
         secondary_scan[secondary_state.index(sub_state)] += (scan_id,)
-        
-g_scan = unravel_list(primary_scan+secondary_scan)
-target_scan = [scan for scan in scan_list if scan not in g_scan]
-target_field = [field for field in field_names if field not in (primary_name+secondary_names)]
+    if has_pol_cal:
+        if sub_state in pol_state:
+            pol_scan[pol_state.index(sub_state)] += (scan_id,)
+    if sub_state in target_state:
+        target_scan+= (scan_id,)
 
 
 with open('config.py','a') as file:
@@ -92,5 +117,9 @@ with open('config.py','a') as file:
     file.write('CAL_1GC_REPHASE'+' = '+str(dorephrase)+'\n')
     file.write('CAL_1GC_PRIMARY_SCAN'+' = '+str(primary_scan)+'\n')
     file.write('CAL_1GC_SECONDARY_SCAN'+' = '+str(secondary_scan)+'\n')
-    file.write('CAL_1GC_TARGET_NAME'+' = '+str(target_field)+'\n')
+    file.write('CAL_1GC_TARGET_NAME'+' = '+str(target_names)+'\n')
     file.write('CAL_1GC_TARGET_SCAN'+' = '+str(target_scan)+'\n')
+    if has_pol_cal:
+        file.write('CAL_1GC_POL_SCAN'+' = '+str(pol_scan)+'\n')
+        file.write('CAL_1GC_POL_NAME'+' = '+str(pol_names)+'\n')
+        file.write('CAL_1GC_POL_STATE'+' = '+str(pol_state)+'\n')
